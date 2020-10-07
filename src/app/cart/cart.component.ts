cafb339b27ba6f51ee3ce4ebf48320bfd9da4512
LeeRuns <klr5240@gmail.com>
2020-10-07 17:33:34 -0600

    All tutorials completed

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from "@angular/core";
+import {FormBuilder } from '@angular/forms';
+
 import { CartService } from "../cart.service";
 
 @Component({
@@ -9,13 +11,31 @@ import { CartService } from "../cart.service";
 export class CartComponent {
   // Member Variables
   items;
+  checkoutForm;
   // Member Functions
 
   // CONSTRUCTOR //
-  constructor(private cartService: CartService) /** inject CartService */
-  { }
+  constructor(
+    private cartService: CartService,  /** inject CartService */
+    private formBuilder: FormBuilder  /** inject FormBuilder Service */
+  ) { 
+
+    /**! Setup form fields to be constructed on view */
+    this.checkoutForm = this.formBuilder.group({
+      name: '',
+      address: ''
+    })
+  }
 
   ngOnInit() {
     this.items = this.cartService.getItems();
+  }
+
+  onSubmit(customerData) {
+    //  Processes checkout data here
+      this.items = this.cartService.clearCart();
+      this.checkoutForm.reset();
+
+      console.warn('Your order ahd been submitted', customerData);
   }
 }
